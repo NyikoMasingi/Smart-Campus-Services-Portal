647a84ddd9a8a52146c64f25173571546dbad184
LindaZwane7 <lindazikode7@gmail.com>
2025-05-05 21:32:45 +0200

    feat: Implement Resource model with types and constructor for resource management

---
+++ server/src/models/resource.model.ts
@@ -0,0 +1,25 @@
+import { ObjectId } from "mongodb";
+ 
+
+export type ResourceType =  'room' | 'equipment' | 'vehicle' | 'other';
+export type ResourceStatus = 'available' | 'booked' | 'maintenance' | 'inactive';
+
+export default class Resource {
+    public resource_id?: ObjectId;
+    public name: string;
+    public type: ResourceType;
+    public status: ResourceStatus;
+    public location: string;
+
+    constructor(
+        name: string,
+        type: ResourceType,
+        status: ResourceStatus,
+        location: string
+    ){
+        this.name = name;
+        this.type = type;
+        this.status = status;
+        this.location = location;
+    }
+}
