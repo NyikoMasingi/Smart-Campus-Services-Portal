374e4a881bb93b89aff492f08925aad5fbe3cd0d
MukhethoniPM <mukhethoni081@gmail.com>
2025-05-17 02:15:45 +0200

    Testing

---
+++ client/jest.config.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+        diagnostics: {
+          ignoreCodes: [2550] 
+        }
+      }
+    ],
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@tanstack|react-router-dom)/)'
+  ]
+};
